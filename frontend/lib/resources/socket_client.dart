924d6e2dd52759863f84d2fe91e3e0daedecab80
Yogesh-03 <yadav.56@iitj.ac.in>
2024-01-21 00:01:47 +0530

    initial commit

---
+++ frontend/lib/resources/socket_client.dart
@@ -0,0 +1,20 @@
+import 'package:mp_tictactoe/credentials/credentials.dart';
+import 'package:socket_io_client/socket_io_client.dart' as IO;
+
+class SocketClient {
+  IO.Socket? socket;
+  static SocketClient? _instance;
+
+  SocketClient.internal(){
+    socket = IO.io('http://${Credentials.myIp}:3000', <String, dynamic>{
+      'transports': ['websocket'],
+      'autoConnect': false
+    });
+    socket!.connect();
+  }
+
+  static SocketClient get instance {
+    _instance ??=SocketClient.internal();
+    return _instance!;
+  }
+}
